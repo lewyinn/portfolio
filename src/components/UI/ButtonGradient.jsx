5ba80d69800ebab16c3030909b7a444c039c62f6
frenxyy <kurniawanridho500@gmail.com>
2024-12-18 12:12:59 +0700

    Update Color Font

--- src/components/UI/ButtonGradient.jsx
+++ src/components/UI/ButtonGradient.jsx
@@ -56,7 +56,7 @@ export function HoverBorderGradient({
         >
             <div
                 className={classNames(
-                    "w-auto text-white z-10 bg-black px-4 py-2 rounded-[inherit]",
+                    "w-auto text-black dark:text-white z-10 bg-black px-4 py-2 rounded-[inherit]",
                     className
                 )}
             >
