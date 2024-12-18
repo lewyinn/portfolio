a1b25d412a02368774e7e0ae0f876cf5ee9d2ed2
frenxyy <kurniawanridho500@gmail.com>
2024-12-18 11:53:15 +0700

    install Analisis vercel

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home.jsx'
 import About from './pages/about.jsx'
 import Projects from './pages/projects.jsx'
 import { ThemeProvider } from './libs/ThemeContext.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/project" element={<Projects />} />
+          <SpeedInsights />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
